33790a819aeffb246554fe637be33ec827c0decf
k2344 <k2344@ELEK-436-WS-25>
2017-09-06 10:57:35 +0300

    Lab01 created and new Labra Project added

--- Hello/BlackJack.cs
+++ Hello/BlackJack.cs
@@ -14,25 +14,33 @@ namespace Hello
             int theirNumber;
             System.Console.WriteLine("*** BlackJack! ***");
             System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-            //reading and converting 
-            theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-            //comparing that given umber is valid
-            if (theirNumber < 1 || theirNumber > 21)
+            //reading and converting
+
+            do
             {
-                Console.WriteLine("The given number is out of limits, try again.");
-            }
-            else
-            {
-                //comparing
-                if (theirNumber >= myNumber && theirNumber <= 21)
+                theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+                //comparing that given umber is valid
+
+
+
+                if (theirNumber < 1 || theirNumber > 21)
                 {
-                    System.Console.WriteLine("You win.");
+                    Console.WriteLine("The given number is out of limits, try again between 1-21.");
                 }
                 else
                 {
-                    System.Console.WriteLine("You lose.");
+                    //comparing
+                    if (theirNumber >= myNumber && theirNumber <= 21)
+                    {
+                        System.Console.WriteLine("You win.");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("You lose.");
+                    }
                 }
-            }
+            } while (theirNumber < 1 || theirNumber > 21);
+
         }
     }
 }
