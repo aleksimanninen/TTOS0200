beb699426ccd4061f6f5ba0a2b0583bd9a98e391
k2344 <k2344@ELEK-436-WS-25>
2017-09-20 11:02:07 +0300

    Lab3 started

---
+++ Lab3/tehtava1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+      public static class Luku
+    {
+       //static bool number { get; set; }
+       //static bool date { get; set; }
+
+     public static bool Onkoluku(string syote)
+        {
+            if (syote == "testi")
+            {
+                Console.WriteLine("TOimiiiii");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+     public static bool OnkoPvm(string syote)
+        {
+            return false;
+        }
+
+    }
+}
