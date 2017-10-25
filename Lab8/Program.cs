e93878f865883015887b4b4362bbfb53d77a3ba1
k2344 <k2344@ELEK-436-WS-25>
2017-10-25 10:50:38 +0300

    Lab8 started

---
+++ Lab8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KÄYTTÄÄKÄÄ AINA TRY-CATCHIÄ
+            try
+            {
+                string nimi;
+                string filupath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filu = filupath + @"\testi.txt";
+                //kirjoitetaan käyttäjän antamat rivit tiedostoon
+                //luodaan StreamWriter tyypiinen olio johon kirjoitetaan
+
+                StreamWriter sw = new StreamWriter(filu);
+                do
+                {
+                    Console.Write("Anna nimi (Enter lopettaa):\n");
+                    nimi = Console.ReadLine();
+                    if (nimi.Length > 0)
+                        sw.WriteLine(nimi);
+                } while (nimi.Length != 0);
+                sw.Close();
+
+                // avataan tiedosto uudestaan ja luetaan sen sisältö ja näytetään
+                // tarkistetaan löytyykö tiedosto
+                if (File.Exists(filu))
+                {
+                    string teksti = File.ReadAllText(filu);
+                    Console.WriteLine(teksti);
+                }
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("Tiedostoa ei löydy!"); // yleensä yksi exception riittää!
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+               
+            }
+    
+
+            
+        }
+    }
+}
